a467ecc3f3264d6613d3c42ba16ba36d3da37922
Mohammad Hossein Ghorbani <esighorbani72@gmail.com>
2025-04-26 19:58:18 +0330

    Attempt to fix docker volume problem for database

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from hokm_bot import app
 from telegram import Update
+import os
 
 if __name__ == "__main__":
+    if 'groups.db' not in os.listdir('./hokm_bot/database/'):
+        with open('./hokm_bot/database/groups.db'):
+            pass
+
     print("Starting the bot...")
     app.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True)
